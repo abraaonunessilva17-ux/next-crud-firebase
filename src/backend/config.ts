e263e204e77586ca3382c6410ba0c4e0842c54eb
abraaonunessilva17-ux <abraao.nunessilva17@gmail.com>
2026-04-25 21:22:30 -0300

    fix firebase config

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -2,7 +2,7 @@
 
 //1. Inicializar o "Túnel" de Comunicação. O Firebase não fica conectado o tempo todo. O config.ts roda assim que o seu app inicia para abrir esse túnel. Ele usa as credenciais que você colocou no .env.local para provar que aquele site tem permissão para mexer no banco de dados "X".
 
-//2. Evitar a Reinicialização (O erro do Next.js). Lembra que falamos do if (!firebase.apps.length)? O config.ts é o lugar onde essa lógica mora. Ele garante que, mesmo que você edite o código 50 vezes e a página dê "refresh", o app só se conecte ao Firebase uma única vez, economizando memória e evitando erros chatos no console.
+//2. Evitar a Reinicialização (O erro do Next.js). Lembra que falamos do if (!firebase.apps.length)? O config.ts é o lugar onde essa lógica mora. Ele garante que, mesmo que você edite o código 50 vezes e a página dê "refresh", o app só se conecte ao Firebase uma única vez, economiza/ndo memória e evitando erros chatos no console.
 
 //3. Exportar as Ferramentas Prontas. A parte mais importante é o export default. Em vez de você ter que configurar o Firebase em cada página do seu site, você faz tudo uma vez só no config.ts e exporta o banco já "pronto para o uso".
 
@@ -15,6 +15,7 @@ if(!firebase.apps.length) { //Quer dizer: A lista de apps inicializados está va
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, //Endereço oficial do seu projeto no Firebase (geralmente usado para sistemas de login).
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID, //É o ID único do seu projeto.
     })
+    console.log("PROJECT ID:", process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID);
 }
 
 export default firebase;
